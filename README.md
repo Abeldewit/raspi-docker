6dfedb24970514e7657466b59bbd31e987e44916
abeldewit <abeldewit@gmail.com>
2023-09-19 10:19:12 +0200

    Script and README update - Added some text output for fresh_install - Extended README with script instructions

--- README.md
+++ README.md
@@ -3,12 +3,21 @@ In this repo, all necessary configurations are stored to setup a clean install o
 
 Using `docker-compose` I configure and run several services in my home. 
 
+# Install and run the container stack
+Using the `fresh_install.sh` script, a fresh Raspbian installation can be configured with the docker service, mounting the NAS necessary for the Media Services listed below, and setting up a `systemctl` service to automatically run the stack on boot. 
+
+The IP for the NAS is asked during this script. 
+
+Please change any file-paths in the `docker-compose.service` file to point to the correct `docker-compose.yaml`. 
+
 ### Services
 - Radarr
 - Sonarr
-- SabNZBD
-- Transmission
+- Bazarr
+- Jackett
+- Flaresolverr
 - Homebridge
-- Portainer
+- Tailscale
+
 
 
--- fresh_install.sh
+++ fresh_install.sh
@@ -1,21 +1,27 @@
 #bin/bash
 
 # Mount the NAS drive to the raspberry pi
+echo "Mounting NAS"
 read -p "Enter NAS IP: " NAS_IP
 sudo echo "$NAS_IP:/volume1/Media /mnt/NAS nfs defaults 0 1" >> /etc/fstab
+sudo mount -a
 
 # Update and install
+echo "*************************************************"
+echo "Updating packages"
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Install docker
+echo "*************************************************"
+echo "Installing docker ..."
+
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-sudo systemctl start docker.service
 
 # Set up Docker's Apt repository:
 echo \
@@ -24,9 +30,12 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo systemctl start docker.service
 
 # Set up the systemd service to start the docker-compose file
+echo "*************************************************"
+echo "Setting up systemctl for docker-compose file"
 sudo cat docker-compose.service > /etc/systemd/system/docker-compose.service
 sudo systemctl daemon-reload
 sudo systemctl enable docker-compose.service
