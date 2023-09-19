0260ef9c9febfdfc3709a35d52a2c3807c8e7ead
abeldewit <abeldewit@gmail.com>
2023-09-19 18:50:55 +0200

    Added flow control to fresh install script

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,52 +1,67 @@
 #bin/bash
 
+DIVIDER="*************************************************"
+
+# Select which parts to execute
+echo "### Raspberry Home Setup ###"
+echo "1. Mount NAS"
+echo "2. Update system"
+echo "3. Install docker"
+echo "4. Install dc stack service"
+echo "5. Install backup cron job"
+echo "6. All of the above"
+read -p "Choice: " FLOW_CHOICE
+
 # Mount the NAS drive to the raspberry pi
-echo "Mounting NAS"
-read -p "Enter NAS IP: " NAS_IP
-sudo echo "$NAS_IP:/volume1/Media /mnt/NAS nfs defaults 0 1" >> /etc/fstab
-sudo mount -a
+if [[ $FLOW_CHOICE == "1" ]] || [[ $FLOW_CHOICE == "6" ]]
+then
+  echo "Mounting NAS"
+  read -p "Enter NAS IP: " NAS_IP
+  sudo echo "$NAS_IP:/volume1/Media /mnt/NAS nfs defaults 0 1" >> /etc/fstab
+  sudo mount -a
+fi
 
 # Update and install
-echo "*************************************************"
-echo "Updating packages"
-sudo apt-get update
-sudo apt-get upgrade -y
+if [[ $FLOW_CHOICE == "2" ]] || [[ $FLOW_CHOICE == "6" ]]
+then
+  echo $DIVIDER
+  echo "Updating packages"
+  sudo apt-get update
+  sudo apt-get upgrade -y
+fi
 
 # Install docker
-echo "*************************************************"
-echo "Installing docker ..."
+if [[ $FLOW_CHOICE == "3"]] || [[ $FLOW_CHOICE == "6" ]]
+then
+  echo $DIVIDER
+  echo "Installing docker ..."
 
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get install -y ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-
-# Set up Docker's Apt repository:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install -y docker docker-compose
-sudo systemctl start docker.service
+  curl -sSL https://get.docker.com | sh
+  sudo usermod -aG docker $USER
+fi
 
 # Set up the systemd service to start the docker-compose file
-echo "*************************************************"
-echo "Setting up systemctl for docker-compose file"
+if [[ $FLOW_CHOICE == "4"]] || [[ $FLOW_CHOICE == "6" ]]
+then
+  echo $DIVIDER
+  echo "Setting up systemctl for docker-compose file"
 
-# next using the defined path, create the service to always boot the compose file
-# Use cat to read the systemd service file and pipe it to sed
-cat docker-compose.service | sed "s|%I|$(pwd)|g" > /tmp/docker-compose.service
-sudo mv /tmp/docker-compose.service /etc/systemd/system/docker-compose.service
+  # next using the defined path, create the service to always boot the compose file
+  # Use cat to read the systemd service file and pipe it to sed
+  cat docker-compose.service | sed "s|%I|$(pwd)|g" > /tmp/docker-compose.service
+  sudo mv /tmp/docker-compose.service /etc/systemd/system/docker-compose.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable docker-compose.service
-sudo systemctl start docker-compose.service
+  sudo systemctl daemon-reload
+  sudo systemctl enable docker-compose.service
+  sudo systemctl start docker-compose.service
+fi
 
 # Set up the daily backup for the service config files
-(crontab -l ; echo "0 0 * * * $(pwd)/backup/backup-script.sh") | crontab -
+if [[ $FLOW_CHOICE == "5" ]] || [[ $FLOW_CHOICE == "6" ]]
+then
+  echo $DIVIDER
+  echo "Adding backup script to cron..."
+  (crontab -l ; echo "0 0 * * * $(pwd)/backup/backup-script.sh") | crontab -
+fi
 
-echo "Finished setup! Docker services are running"
+echo "Finished setup."
